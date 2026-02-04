d94fbbf799b10b8ac0c6c0c84b22b6116a48c2d8
Anushka <anushkajagtap2212@gmail.com>
2026-02-04 20:44:24 +0530

    Unique Path file

---
+++ Recursion_Part2/UniquePath.java
@@ -0,0 +1,13 @@
+public class UniquePath {
+
+    public static int unique(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        return unique(m - 1, n) + unique(m, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(unique(3, 3));
+    }
+}
