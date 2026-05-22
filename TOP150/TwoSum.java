487ded38f3004f4e109d90a7acf5f4460a48813b
Anushka <anushkajagtap2212@gmail.com>
2026-05-22 13:06:27 +0530

    TOP 150

--- TOP150/TwoSum.java
+++ TOP150/TwoSum.java
@@ -17,4 +17,18 @@ public class TwoSum {
         }   
         return new int[]{};
     }
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] result = ts.twoSum(nums, target);
+        if(result.length == 2)
+        {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        }
+        else
+        {
+            System.out.println("No two sum solution found.");
+        }
+    }
 }
