16891a87b4b50ced39bbd78fb3607cc50b329727
Anushka <anushkajagtap2212@gmail.com>
2026-07-27 21:17:38 +0530

    PATTERN 4

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,19 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for(int row = 1 ; row <= n ; row++)
+        {
+            //spaces
+            for(int col = 1 ; col <= n - row ; col ++)
+            {
+                System.out.print(" ");
+            }
+            //stars
+            for(int col = 1 ; col <= n ; col ++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }    
+}
